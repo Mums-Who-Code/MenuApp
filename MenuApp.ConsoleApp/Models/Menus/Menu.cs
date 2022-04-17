9d42bbdd677df58b6da37934de3b85443ffa9f62
Nayna Mallick <naynamallick@outlook.com>
2022-04-16 19:08:41 -0700

    BROKER: Insert Menu

---
+++ MenuApp.ConsoleApp/Models/Menus/Menu.cs
@@ -0,0 +1,12 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+namespace MenuApp.ConsoleApp.Models.Menus
+{
+    internal class Menu
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}

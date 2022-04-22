4ae3f2f8e825c83b1587ced883c156a029f132c3
Nayna Mallick <naynamallick@outlook.com>
2022-04-22 15:10:24 -0700

    ShouldThrowValidationExceptionOnAddIfSampleIsInvalidAndLogIt -> FAIL

---
+++ MenuApp.ConsoleApp/Models/Menus/Exceptions/InvalidMenuException.cs
@@ -0,0 +1,15 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Xeptions;
+
+namespace MenuApp.ConsoleApp.Models.Menus.Exceptions
+{
+    public class InvalidMenuException : Xeption
+    {
+        public InvalidMenuException()
+            : base(message: "Menu is invalid, please fix the error and try again")
+        { }
+    }
+}
--- MenuApp.Tests.Unit/Services/Foundations/Menus/MenuServiceTests.Validations.Add.cs
+++ MenuApp.Tests.Unit/Services/Foundations/Menus/MenuServiceTests.Validations.Add.cs
@@ -35,5 +35,54 @@ namespace MenuApp.Tests.Unit.Services.Foundations.Menus
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfSampleIsInvalidAndLogIt(
+            string invalidText)
+        {
+            //given
+            Menu invalidMenu = new Menu
+            {
+                ItemName = invalidText
+            };
+
+            var invalidMenuException = new InvalidMenuException();
+
+            invalidMenuException.AddData(
+                key: nameof(Menu.Id),
+                values: "Id is required.");
+
+            invalidMenuException.AddData(
+               key: nameof(Menu.ItemName),
+               values: "Item is required.");
+
+            invalidMenuException.AddData(
+               key: nameof(Menu.Price),
+               values: "Price is required.");
+
+            var expectedMenuValidationException =
+                new MenuValidationException(invalidMenuException);
+
+            //when
+            Action addMenuAction = () => this.menuService.AddMenu(invalidMenu);
+
+            //then
+            Assert.Throws<MenuValidationException>(addMenuAction);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMenuValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertMenu(It.IsAny<Menu>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }

c2e53fefe67727304c3c29b0a3f9a6e2b7c6f2c3
Nayna Mallick <naynamallick@outlook.com>
2022-04-23 09:43:41 -0700

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogIt -> FAIL

---
+++ MenuApp.ConsoleApp/Models/Menus/Exceptions/MenuServiceException.cs
@@ -0,0 +1,16 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using Xeptions;
+
+namespace MenuApp.ConsoleApp.Models.Menus.Exceptions
+{
+    public class MenuServiceException : Xeption
+    {
+        public MenuServiceException(Xeption innerException)
+            : base(message: "Menu serivce error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
---
+++ MenuApp.Tests.Unit/Services/Foundations/Menus/MenuServiceTests.Exceptions.Add.cs
@@ -0,0 +1,47 @@
+﻿using MenuApp.ConsoleApp.Models.Menus;
+using MenuApp.ConsoleApp.Models.Menus.Exceptions;
+using Moq;
+using Xunit;
+
+namespace MenuApp.Tests.Unit.Services.Foundations.Menus
+{
+    public partial class MenuServiceTests
+    {
+        [Fact]
+        public void ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogIt()
+        {
+            // given
+            Menu inputMenu = CreateRandomMenu();
+            var serviceException = new Exception();
+
+            var failedMenuServiceException =
+                new FailedMenuServiceException(serviceException);
+
+            var expectedMenuServiceException =
+                new MenuServiceException(
+                    failedMenuServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertMenu(It.IsAny<Menu>()))
+                    .Throws(serviceException);
+
+            // when
+            Action addMenuAction = () => this.menuService.AddMenu(inputMenu);
+
+            // then
+            Assert.Throws<MenuServiceException>(addMenuAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertMenu(It.IsAny<Menu>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMenuServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

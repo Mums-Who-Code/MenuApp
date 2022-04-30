a4a573f4a126f599f1f1cd6875a61300de05c4ff
Nayna Mallick <naynamallick@outlook.com>
2022-04-30 11:32:27 -0700

    ShouldThrowDependencyValidationExceptionOnRetrieveByIdIfValidationErrorOccursAndLogIt -> FAIL

---
+++ MenuApp.Tests.Unit/Services/Foundations/Menus/MenuServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,47 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using MenuApp.ConsoleApp.Models.Menus.Exceptions;
+using Moq;
+using Xunit;
+
+namespace MenuApp.Tests.Unit.Services.Foundations.Menus
+{
+    public partial class MenuServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyValidationExceptionOnRetrieveByIdIfValidationErrorOccursAndLogIt()
+        {
+            //given
+            int someMenuId = GetRandomNumber();
+            var argumentNullException = new ArgumentNullException();
+
+            var nullArgumentMenuException =
+                new NullArgumentMenuException(argumentNullException);
+
+            var expectedMenuDependencyValidationException =
+                new MenuDepecdencyValidationException(
+                    nullArgumentMenuException);
+
+            //when
+            Action retrieveMenuByIdAction = () =>
+                this.menuService.RetrieveMenuById(someMenuId);
+
+            //then
+            Assert.Throws<MenuDepecdencyValidationException>(retrieveMenuByIdAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectMenuById(It.IsAny<int>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMenuDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

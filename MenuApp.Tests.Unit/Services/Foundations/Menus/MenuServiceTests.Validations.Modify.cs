623d720444737af5a4a80fb2c0b8ffe505c67b39
Nayna Mallick <naynamallick@outlook.com>
2022-05-11 10:35:41 -0700

    ShouldThrowValidationExceptionOnModifyIfSampleIsNullAndLogIt -> FAIL

---
+++ MenuApp.Tests.Unit/Services/Foundations/Menus/MenuServiceTests.Validations.Modify.cs
@@ -0,0 +1,39 @@
+﻿using MenuApp.ConsoleApp.Models.Menus;
+using MenuApp.ConsoleApp.Models.Menus.Exceptions;
+using Moq;
+using Xunit;
+
+namespace MenuApp.Tests.Unit.Services.Foundations.Menus
+{
+    public partial class MenuServiceTests
+    {
+        [Fact]
+        public void ShouldThrowValidationExceptionOnModifyIfSampleIsNullAndLogIt()
+        {
+            //given
+            Menu nullMenu = null;
+            var nullMenuException = new NullMenuException();
+
+            var expectedMenuValidationException =
+                new MenuValidationException(nullMenuException);
+
+            //when
+            Action modifyMenuAction = () => this.menuService.ModifyMenu(nullMenu);
+
+            //then
+            Assert.Throws<MenuValidationException>(modifyMenuAction);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMenuValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateMenu(It.IsAny<Menu>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}

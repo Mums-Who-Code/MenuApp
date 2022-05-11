82f6f11addf78e2eb8b96d856a2ca51728d66d7a
Nayna Mallick <naynamallick@outlook.com>
2022-05-11 10:59:23 -0700

    ShouldThrowValidationExceptionOnModifyIfMenuIsInvalidAndLogIt -> FAIL

--- MenuApp.Tests.Unit/Services/Foundations/Menus/MenuServiceTests.Validations.Modify.cs
+++ MenuApp.Tests.Unit/Services/Foundations/Menus/MenuServiceTests.Validations.Modify.cs
@@ -35,5 +35,54 @@ namespace MenuApp.Tests.Unit.Services.Foundations.Menus
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ShouldThrowValidationExceptionOnModifyIfMenuIsInvalidAndLogIt(
+            string invalidText)
+        {
+            //given
+            var invalidMenu = new Menu
+            {
+                ItemName = invalidText
+            };
+
+            var invalidMenuException = new InvalidMenuException();
+
+            invalidMenuException.AddData(
+               key: nameof(Menu.Id),
+               values: "Id is required.");
+
+            invalidMenuException.AddData(
+               key: nameof(Menu.ItemName),
+               values: "ItemName is required.");
+
+            invalidMenuException.AddData(
+               key: nameof(Menu.Price),
+               values: "Price is required.");
+
+            var expectedMenuValidationException =
+                new MenuValidationException(invalidMenuException);
+
+            //when
+            Action modifyMenuAction = () => this.menuService.ModifyMenu(invalidMenu);
+
+            //then
+            Assert.Throws<MenuValidationException>(modifyMenuAction);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedMenuValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateMenu(It.IsAny<Menu>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
